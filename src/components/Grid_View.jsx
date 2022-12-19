45f07cdd49c7a3bf370c48ac25748d5117abeb5f
Shreyas <dongareshreyas@gmail.com>
2022-12-19 15:38:29 +0530

    Grid View File changed

---
+++ src/components/Grid_View.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Product from "./Product";
+
+const Grid_View = ({ products }) => {
+  return (
+    <div className="grid grid-cols-2 md:grid-cols-3">
+      {products.map((item) => {
+        return (
+          <div key={item.id} className="px-4 w-52 mx-auto md:w-72 my-4">
+            <Product item={item} />
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Grid_View;
