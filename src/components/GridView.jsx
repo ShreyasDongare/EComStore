fa73aa0cfddd57bff45253153e3576fa6cbc9516
Shreyas <dongareshreyas@gmail.com>
2022-12-19 14:21:47 +0530

    GridView file

--- src/components/GridView.jsx
+++ src/components/GridView.jsx
@@ -5,9 +5,9 @@ const GridView = ({products}) => {
   return (
     <div className='grid grid-cols-2 md:grid-cols-3'>
       {
-        products.map((item, index)=>{
+        products.map((item)=>{
             return (
-              <div key={item.id} className="px-4 w-52 mx-auto md:w-72  my-4">
+              <div key={item.id} className="px-4 w-52 mx-auto md:w-72 my-4">
                 <Product item={item} />
               </div>
             );
